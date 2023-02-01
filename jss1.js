e03bd56a7a1062de69f920eb1bcd07a1e691b31c
ayildirim34 <ayildirim3457@protonmail.com>
2023-02-01 08:44:44 +0100

    aa

---
+++ jss1.js
@@ -0,0 +1,32 @@
+// var person = {
+//     fistname : "Ahmet",
+//     lastname : "Yildirim",
+//     age :42,
+// };
+// console.log(person);
+// let a=2;
+// console.log(a);
+
+let age =20;
+let age1 =age;
+
+console.log (`yas : ${age}`)
+
+age=30;
+
+console.log(age)
+
+var info = {
+    isim : "Mehmet",
+    yas : 35
+};
+
+console.log(`Ismi : ${info.isim} yasi: ${info.yas}`);
+
+var info1=info;
+
+info1.yas=40;
+
+console.log(`Ismi : ${info.isim} yasi: ${info.yas}`);
+
+var cars = ["BMW0", "Mer"]
